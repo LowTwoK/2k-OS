cebb2eadb2afeb6fc6957fe4574d3128f1c0c293
low2k <comlex5@zohomail.com>
2026-09-05 18:22:23 +0700

    Fix lower letter some special char still bug

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -4,8 +4,9 @@ void twok_main()
 {
     clear(15);
 
-    draw_rect(0, 0, 640, 30, 1);
+    draw_rect(0, 0, 640, 60, 1);
 
     draw_string("2k OS By Low2k", 10, 10, 15);
     draw_string("ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz", 10, 20, 15);
+    draw_string("1234567890 +-/*=()&!@#%<>?{}[];:~""", 10, 40, 15);
 }
